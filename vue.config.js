bc1b29149507813b7e21c814690a5df4543afdfe
Sergei Kornienko <qluqua@gmail.com>
2018-07-18 20:34:45 +0300

    config files

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: [
+    'dom7',
+    'swiper',
+  ],
+  productionSourceMap: false,
+  // configureWebpack: {
+  //   plugins: [
+  //     new MyAwesomeWebpackPlugin()
+  //   ]
+  // }
+};
