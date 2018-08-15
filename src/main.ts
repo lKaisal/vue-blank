4ba1ff3fb60a779091588c0e554c6f1bea01aec4
Sergei Kornienko <qluqua@gmail.com>
2018-08-15 17:20:42 +0300

    savepoint

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'whatwg-fetch' // native js fetch polyfill
 import 'svgxuse' // ie 11 svg use fix
 
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 // import '@/registerServiceWorker'
