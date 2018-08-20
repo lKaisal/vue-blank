7f26e0a6315252370c8671125939e398023f0a9b
lKaisal <37440366+lKaisal@users.noreply.github.com>
2018-08-20 14:22:43 +0300

    Update scrollTo.ts

--- src/mixins/scrollTo.ts
+++ src/mixins/scrollTo.ts
@@ -2,10 +2,10 @@ function calcTop(el) {
   return el.getBoundingClientRect().top
 }
 
-/** Функция вертикальной прокрутки на заданное расстояние или до указанного HTML-элемента 
- * !!! Для IE применяется scrollIntoView вместо scrollTo, т.е. скролл на IE будет
- * срабатывать только если передан объект, а не число.
-*/
+  /** Функция вертикальной прокрутки на заданное расстояние или до указанного HTML-элемента
+   * !!! Для IE применяется scrollIntoView вместо scrollTo, т.е. скролл на IE будет
+   * срабатывать только если передан объект, а не число.
+   */
 function scrollTo(
   /** Число, строка или объект (HTML-элемент)
    * Если объект - рассчитывается расстояние от края экрана до верхней границы элемента
@@ -13,7 +13,7 @@ function scrollTo(
    * Если число(строка) - скроллит на заданное число px.
    * Значение по умолчанию - для прокрутки к началу страницы
    */
-  payload: any = 'html',
+  payload: any = 0,
   /** Отступ от верхнего края экрана */
   gutter: number = 0
 ) {
@@ -30,13 +30,20 @@ function scrollTo(
    * Значение если InternetExplorer: true
    */
   const isEdge = navigator.userAgent.search('Edge')
-  const isIE = (navigator.userAgent.indexOf(".NET") != -1)
+  const isIE = (navigator.userAgent.indexOf('.NET') !== -1)
 
   /** Функция для IE */
   if (isIE) {
-    document.querySelector(payload).scrollIntoView({behavior: 'smooth'})
+    if (payload !== 0) {
+      const payloadClass = payload.classList[0]
+      document.getElementsByClassName(payloadClass)[0].scrollIntoView({behavior: 'smooth'})
+      return
+    }
+
+    document.querySelector('html').scrollIntoView({behavior: 'smooth'})
   }
 
+  /** Функция для IE */
   if ( isEdge > 0) {
     window.scroll(0, top)
     return
