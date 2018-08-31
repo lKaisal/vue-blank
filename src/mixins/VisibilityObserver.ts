3791ff27d7fe18b33661b7bf713ad94f74e284f4
lKaisal <37440366+lKaisal@users.noreply.github.com>
2018-08-31 19:24:23 +0300

    Update VisibilityObserver.ts

--- src/mixins/VisibilityObserver.ts
+++ src/mixins/VisibilityObserver.ts
@@ -3,11 +3,14 @@
 import 'intersection-observer' // IE polyfill for Intersection Observer API
 
 interface Options {
-  /** HTML-элемент, который должен появиться в зоне видимости для вызова коллбэка */
-  target: HTMLElement,
-  /** Отступ от края элемента - величина видимой части элемента для вызова коллбэка */
-  offset: number,
-  callback: () => void
+  /** HTML-элемент, который должен появиться в зоне видимости для вызова коллбэка ifIntoView */
+  target: HTMLElement
+  /** Отступ от края элемента - величина видимой части элемента для вызова коллбэка ifIntoView */
+  offset: number
+  /** Коллбэк, срабатывающий когда элемент (target) появляется в зоне видимости с учетом offset */
+  ifIntoView?: () => void
+  /** Коллбэк, срабатывающий когда target находится вне зоны видимости с учетом offset */
+  ifOutOfView?: () => void
 }
 
 export default class VisibilityObserver {
@@ -24,7 +27,13 @@ export default class VisibilityObserver {
       const target = targets[0];
 
       if (target.isIntersecting) {
-        this.options.callback()
+        if ('ifIntoView' in this.options) {
+          this.options.ifIntoView()
+        }
+      } else {
+        if ('ifOutOfView' in this.options) {
+          this.options.ifOutOfView()
+        }
       }
     }, {
       rootMargin: `-${this.options.offset}px`,
